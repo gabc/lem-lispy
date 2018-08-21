9a3179a3b837fe2cbe96232c085f260795e74e53
Gabriel Beauchamp <gabriel@m32.media>
2018-08-21 19:41:13 -0400

    More features

--- lem-lispy.lisp
+++ lem-lispy.lisp
@@ -7,7 +7,7 @@
      :keymap *lispy-mode-keymap*))
 
 (defun at-open-p (&optional (point (current-point)))
-  (looking-at point "\\("))
+  (char= #\( (character-at point)))
 
 (define-command lispy-down () ()
   (if (at-open-p)
@@ -15,19 +15,44 @@
         (forward-sexp)
         (forward-sexp)
         (backward-sexp))
-      (insert-character (current-point) #\j)))
+      (self-insert 1)))
 
 (define-command lispy-up () ()
   (if (at-open-p)
-      (progn
-        (backward-sexp))        
-      (insert-character (current-point) #\k)))
+        (backward-sexp)
+      (self-insert 1)))
 
 (define-command lispy-comment-sexp () ()
   (when (at-open-p)
     (mark-sexp))
   (lem.language-mode::comment-or-uncomment-region))
 
+(define-command lispy-mark-list () ()
+  (if (at-open-p)
+      (mark-sexp)
+      (self-insert 1)))
+
+(defun region-active (&optional (buffer (current-buffer)))
+  (buffer-mark-p buffer))
+
+(defun surround ()
+  (let ((start (current-point))
+        (end (current-point)))
+    (when (region-active)
+      (setf start (region-beginning)
+            end (region-end)))
+    (insert-character start #\( )
+    (forward-sexp 1)
+    (insert-character end #\) )
+    (backward-sexp 1)))
+
+(define-command lispy-surround () ()
+  (if (at-open-p)
+      (surround)
+      (self-insert 1)))
+
 (define-key *lispy-mode-keymap* "M-;" 'lispy-comment-sexp)
 (define-key *lispy-mode-keymap* "j" 'lispy-down)
 (define-key *lispy-mode-keymap* "k" 'lispy-up)
+(define-key *lispy-mode-keymap* "m" 'lispy-mark-list)
+(define-key *lispy-mode-keymap* "s" 'lispy-surround)
